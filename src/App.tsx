3d88b9ca025abc84f2234eee07b33eef6c10b405
RB-BRAHIAM <Vrbrahiam23@gmail.com>
2024-10-07 01:06:18 -0500

    Quinto Commit FrondEnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import './App.css'
+import './App.css';
 import NavBar from "./components/NavBar";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
@@ -19,6 +19,7 @@ function App() {
     <Router>
       <NavBar brandName="KarCamilo" navItems={items} />
       <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,20 +1,44 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '../style/home.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
     return (
         <div>
-            <h1>Bienvenido a la Tienda</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/login">Iniciar Sesión</Link>
-                    </li>
-                    <li>
-                        <Link to="/register">Registrarse</Link>
-                    </li>
-                </ul>
-            </nav>
+            <div className='divHome'>
+                <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img className="d-block w-100" src="/bmw.png" alt="First slide" />
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="/Camaro.png" alt="Second slide" />
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="/Mercedes.png" alt="Third slide" />
+                        </div>
+                    </div>
+                    <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </a>
+                    <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </a>
+                </div>
+            </div>
+
+            <div className="card" style={{ width: '20rem' }}>
+                <img className="card-img-top" src="/bmw.png" alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">BMW</h5>
+                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic deserunt quas sit porro aut, id beatae alias deleniti, quidem facilis, fuga assumenda sequi corrupti. Tenetur molestiae culpa alias dolore repellat!</p>
+                    <Link to="#" className="btn btn-primary">Go somewhere</Link>
+                </div>
+            </div>
         </div>
     );
 };
